4446a9ee2d389bff6fe3e16762d81583cb8d15e8
Abdallah Ibrahim Zeiada <84587952+abdallahzeiada@users.noreply.github.com>
2025-05-15 22:50:12 +0300

    fix: simplify BASE_URL definition for axios instance

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const BASE_CLIENT_URL =
   import.meta.env.VITE_BASE_CLIENT_URL || "http://localhost:5173";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? `${BASE_CLIENT_URL}/api` : "/api";
+const BASE_URL = `${BASE_CLIENT_URL}/api`;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
