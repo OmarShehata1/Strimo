b0029c6e259437c6af94d3dbdda8014f358936cd
Abdallah Ibrahim Zeiada <84587952+abdallahzeiada@users.noreply.github.com>
2025-05-15 22:41:45 +0300

    feat: update axios configuration and server CORS settings; add client vercel.json

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5173/api" : "/api";
+const BASE_CLIENT_URL =
+  import.meta.env.VITE_BASE_CLIENT_URL || "http://localhost:5173";
+
+const BASE_URL =
+  import.meta.env.MODE === "development" ? `${BASE_CLIENT_URL}/api` : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
--- server/src/server.js
+++ server/src/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONEND_URL,
     credentials: true,
   })
 );
